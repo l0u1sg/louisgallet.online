44cb8bb581a9f7b54731b5fd5ab8d61ce8320f4b
Louis GALLET <46707587+louisgallet95@users.noreply.github.com>
2020-04-10 15:48:13 +0200

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,19 +1,7 @@
-$.urlParam = function(name, url) {
-                if (!url) {
-                    url = window.location.href;
-                }
-                var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
-                if (!results) { 
-                    return undefined;
-                }
-                return results[1] || undefined;
-            }
-            window.onload = function() {
-                var forceDesktop = $.urlParam('forceDesktop');
-                if (!forceDesktop) {
-                    if ( $(window).width() < 639) {   
-                        var url = "https://mobile.louisgallet.online";    
-                        $(location).attr('href',url);
-                    }
-                }
-            };
+<script type="text/javascript">
+<!--
+if (screen.width <= 699) {
+document.location = "mobile.louisgallet.online";
+}
+//-->
+</script>
