f7e9413635bc76bdffabc594109386f4b5278ffd
Louis GALLET <46707587+louisgallet95@users.noreply.github.com>
2020-04-10 15:42:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -439,4 +439,22 @@ $(function() {
   }
 
 });
-
+$.urlParam = function(name, url) {
+                if (!url) {
+                    url = window.location.href;
+                }
+                var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
+                if (!results) { 
+                    return undefined;
+                }
+                return results[1] || undefined;
+            }
+            window.onload = function() {
+                var forceDesktop = $.urlParam('forceDesktop');
+                if (!forceDesktop) {
+                    if ( $(window).width() < 639) {   
+                        var url = "https://mobile.louisgallet.online";    
+                        $(location).attr('href',url);
+                    }
+                }
+            };
